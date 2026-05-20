8891b63a505b2f6e650fd3cacbd6dc87d3de6f6a
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2026-05-20 16:33:31 +0530

    Update glyphix.h
    updated the function declarations

--- include/glyphix.h
+++ include/glyphix.h
@@ -1,4 +1,3 @@
 #pragma once
 
-void gly_display(std::string str,int size,char style,char choice);
-void gly_write(std::string str,int size,char style,char choice);
+std::string glyphix(std::string str,int size,char style);
